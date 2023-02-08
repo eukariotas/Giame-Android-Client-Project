50deee84d1b9971c71c288084ba8dbc63da4bb55
ukanda-works <manuel.amoros.egidio@gmail.com>
2023-02-08 12:44:18 +0000

    feat: base de datos añadida

---
+++ app/src/main/java/es/eukariotas/giame/persistence/database/dao/UserDao.kt
@@ -0,0 +1,17 @@
+package es.eukariotas.giame.persistence.database.dao
+
+import androidx.room.Dao
+
+@Dao
+interface UserDao {
+
+
+    // TODO: metodo para verificar usuario
+    //TODO: metodo para insertar usuario
+    //TODO: metodo para actualizar usuario
+    //TODO: metodo para consultar usuario
+
+
+    //TODO: metodo para verificar token
+    //TODO: metodo para manejar token
+}
---
+++ app/src/main/java/es/eukariotas/giame/persistence/database/entities/UserEntity.kt
@@ -0,0 +1,29 @@
+package es.eukariotas.giame.persistence.database.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+
+@Entity(tableName = "user")
+data class UserEntity(
+    @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "user") val name: String,
+    @ColumnInfo(name = "password") val password: String,
+    @ColumnInfo(name = "email") val email: String,
+    @ColumnInfo(name = "image") val avatar: String,
+    @ColumnInfo(name = "Country") val country: String,
+    @ColumnInfo(name = "description") val city: String,
+    @ColumnInfo(name = "last_login") val lastLogin: String,
+){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as UserEntity
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (email != other.email) return false
+
+        return true
+    }
+}
