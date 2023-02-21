09bce5f42358cf7821a73c6c58bbec706dea4733
RamonPagan <72938346+RamonPagan@users.noreply.github.com>
2023-02-21 16:16:18 +0100

    Textura del mundo con aplicacion Tiled y hitbox del cohete

--- app/src/main/java/es/eukariotas/giame/game/Aviones/Rocket.kt
+++ app/src/main/java/es/eukariotas/giame/game/Aviones/Rocket.kt
@@ -10,50 +10,48 @@ import com.badlogic.gdx.physics.box2d.Fixture
 import com.badlogic.gdx.physics.box2d.FixtureDef
 import com.badlogic.gdx.physics.box2d.PolygonShape
 
-class Rocket:PolygonShape {
+class Rocket//rocketBox = Rectangle()
+//rocketBox!!.width = 64f
+//rocketBox!!.height = 64f
+//posRocket = Vector2(800/2f,480/2f)
+    () : PolygonShape() {
 
     private var bodyDef = BodyDef().apply {
         type = BodyDef.BodyType.DynamicBody
-        position.set(5f, 10f)
+        position.set(50f, 5f)
     }
-
 
     private var imageRocket = Texture(Gdx.files.internal("avion.png"))
-    private var rocketBox: Rectangle? = null
-    private var posRocket: Vector2? = null
+
+    //private var rocketBox: Rectangle? = null
+    //private var posRocket: Vector2? = null
+
     private var rocketShape = PolygonShape().apply {
-        setAsBox((imageRocket!!.width / 2).toFloat(), (imageRocket!!.height / 2).toFloat())
+        setAsBox(2f, 2f)
     }
 
-
-    constructor(){
-        val fixtureDef = FixtureDef()
-        fixtureDef.shape = rocketShape
-        fixtureDef.density = 1f
+    private var rFixtureDef = FixtureDef()
 
 
-        rocketBox = Rectangle()
-        rocketBox!!.width = 64f
-        rocketBox!!.height = 64f
-        posRocket = Vector2(800/2f,480/2f)
-
-        rocketShape?.dispose()
+    init {
+        rFixtureDef.shape = rocketShape
+        rFixtureDef.density = 1f
     }
 
-    fun getRocketShpae(): PolygonShape?{
-        return rocketShape
+    fun getRocketShape(): FixtureDef{
+        return  rFixtureDef
     }
 
-    fun getBodyDef(): BodyDef? {
+    fun getBodyDef(): BodyDef {
         return bodyDef
     }
 
-    fun getRocket(): Texture? {
+    fun getRocket(): Texture {
         return imageRocket
     }
 
-    fun getPosRocket(): Vector2? {
+    /*fun getPosRocket(): Vector2? {
         return posRocket
-    }
+    }*/
 
 }
