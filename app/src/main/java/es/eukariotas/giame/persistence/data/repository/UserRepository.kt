f2e368fa2ad9a67efb3f7083cca591f75a45a8a5
RamonPagan <72938346+RamonPagan@users.noreply.github.com>
2023-02-24 16:06:32 +0100

    Encrypt MD5

--- app/src/main/java/es/eukariotas/giame/persistence/data/repository/UserRepository.kt
+++ app/src/main/java/es/eukariotas/giame/persistence/data/repository/UserRepository.kt
@@ -1,19 +1,22 @@
 package es.eukariotas.giame.persistence.data.repository
 
-import android.app.Application
-import android.content.Context
 import es.eukariotas.giame.persistence.data.apiService.UserService
-import es.eukariotas.giame.persistence.data.apiclient.UserApiClient
-import es.eukariotas.giame.persistence.data.model.UserModel
-import es.eukariotas.giame.persistence.database.UserDatabase
 import es.eukariotas.giame.persistence.database.dao.UserDao
 import es.eukariotas.giame.persistence.database.entities.UserEntity
+import java.security.MessageDigest
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
          private val api:UserService,
          private val userDao: UserDao
 ) {
+
+    @Throws(Exception::class)
+    fun encrypt(data: ByteArray): ByteArray {
+        val md5 = MessageDigest.getInstance("MD5")
+        md5.update(data)
+        return md5.digest()
+    }
 
     suspend fun login(name: String, password: String):UserEntity{
         return api.login(name, password)
@@ -23,4 +26,6 @@ class UserRepository @Inject constructor(
     suspend fun insertUser(repose: UserEntity){
         userDao.insertUser(repose.id, repose.name, repose.password, repose.email, repose.avatar?:"null", repose.country?:"null", repose.description?:"null", repose.lastLogin, repose.token)
     }
+
+
 }
