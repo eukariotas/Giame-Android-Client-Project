e188266e9a14a8deed3c8debff9ee46facd1958e
manuel.tecnico <manuel.tecnico@activaelkoko.com>
2023-03-01 02:49:43 +0100

    mejoras en la comunicacion

--- app/src/main/java/es/eukariotas/giame/persistence/DataBaseProv.kt
+++ app/src/main/java/es/eukariotas/giame/persistence/DataBaseProv.kt
@@ -1,9 +1,12 @@
 package es.eukariotas.giame.persistence
 
+import es.eukariotas.giame.persistence.data.model.PartyModel
 import es.eukariotas.giame.persistence.data.model.UserModel
 
 class DataBaseProv {
     companion object{
+        var partidaActual: PartyModel? = null
+        var playerNum : Int = 0
         var usuario = UserModel(99999999,"invitado","invitado@gmail.com","invitado","","cuenta de invitado","Es","")
         var token = ""
 
